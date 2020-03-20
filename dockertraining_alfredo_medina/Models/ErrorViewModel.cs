d3f30825eba4394f5a753ce26ee5555dd8ea7ca7
unknown <alfmedina@brierley.com>
2020-03-20 12:23:18 -0600

    first commit

---
+++ dockertraining_alfredo_medina/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dockertraining_alfredo_medina.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
